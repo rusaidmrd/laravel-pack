1a54f01089918bafb0e1915cae0391219947b6ce
Rusaid <rusaidmrd@gmail.com>
2023-03-17 20:48:42 +0530

    Migration for post,comment and PostComment

--- database/migrations/2023_03_17_151448_create_post_user_table.php
+++ database/migrations/2023_03_17_151448_create_post_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreatePostUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->json('body')->nullable();
-            $table->foreignId('user_id');
+        Schema::create('post_user', function (Blueprint $table) {
+            $table->foreignId('user_id')->index();
             $table->foreign('user_id')->on('users')->references('id')->cascadeOnDelete();
-            $table->foreignId('post_id');
+            $table->foreignId('post_id')->index();
             $table->foreign('post_id')->on('posts')->references('id')->cascadeOnDelete();
-            $table->timestamps();
+
+            $table->primary(['user_id','post_id']);
         });
     }
 
@@ -31,6 +30,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('post_user');
     }
 }
