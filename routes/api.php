197a0fcf74818c27872d842327cbbddee9c2526c
Rusaid <rusaidmrd@gmail.com>
2023-05-16 21:04:49 +0530

    Remove api versioning feature

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::prefix('v1')
-    ->group(function (){
-        RouteHelper::includeRouteFiles(__DIR__.'/api/v1');
-//      require __DIR__ . '/api/v1/users.php';
-    });
+RouteHelper::includeRouteFiles(__DIR__ . '/api');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
